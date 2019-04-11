a8f9508379d90f1dfb2183c02333cb2847dbef49
HQebupt <qiang.huang1991@gmail.com>
2019-04-11 17:07:23 +0800

    'update'

--- src/main/java/lambdasinaction/chap5/NumericStreams.java
+++ src/main/java/lambdasinaction/chap5/NumericStreams.java
@@ -1,4 +1,5 @@
 package lambdasinaction.chap5;
+
 import lambdasinaction.chap4.*;
 
 import java.util.stream.*;
@@ -6,51 +7,59 @@ import java.util.*;
 
 import static lambdasinaction.chap4.Dish.menu;
 
-public class NumericStreams{
+public class NumericStreams {
 
-    public static void main(String...args){
-    
-        List<Integer> numbers = Arrays.asList(3,4,5,1,2);
+    public static void main(String... args) {
+
+        List<Integer> numbers = Arrays.asList(3, 4, 5, 1, 2);
 
         Arrays.stream(numbers.toArray()).forEach(System.out::println);
         int calories = menu.stream()
-                           .mapToInt(Dish::getCalories)
-                           .sum();
+                .mapToInt(Dish::getCalories)
+                .sum();
         System.out.println("Number of calories:" + calories);
 
 
         // max and OptionalInt
-        OptionalInt maxCalories = menu.stream()                                                      
-                                      .mapToInt(Dish::getCalories)
-                                      .max();
+        OptionalInt maxCalories = menu.stream()
+                .mapToInt(Dish::getCalories)
+                .max();
 
         int max;
-        if(maxCalories.isPresent()){
+        if (maxCalories.isPresent()) {
             max = maxCalories.getAsInt();
-        }
-        else {
+        } else {
             // we can choose a default value
             max = 1;
         }
-        System.out.println(max);
+        System.out.println("max:" + max);
 
         // numeric ranges
         IntStream evenNumbers = IntStream.rangeClosed(1, 100)
-                                 .filter(n -> n % 2 == 0);
+                .filter(n -> n % 2 == 0);
 
         System.out.println(evenNumbers.count());
 
         Stream<int[]> pythagoreanTriples =
-               IntStream.rangeClosed(1, 100).boxed()
+                IntStream.rangeClosed(1, 100).boxed()
                         .flatMap(a -> IntStream.rangeClosed(a, 100)
-                                               .filter(b -> Math.sqrt(a*a + b*b) % 1 == 0).boxed()
-                                               .map(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));       
+                                .filter(b -> Math.sqrt(a * a + b * b) % 1 == 0).boxed()
+                                .map(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}));
 
-        pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2])); 
+        pythagoreanTriples.forEach(t -> System.out.println(t[0] + ", " + t[1] + ", " + t[2]));
+        System.out.println("===========================pythagoreanTriples:");
+        Stream<int[]> pythons = IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100).boxed()
+                        .flatMap(b -> IntStream.rangeClosed(b, 100).boxed()
+                                .map(c -> new int[]{a, b, c}))
+                        .filter(d -> d[0] * d[0] + d[1] * d[1] == d[2] * d[2])
+                );
+        pythons.forEach(t -> System.out.println(t[0] + "," + t[1] + "," + t[2]));
+        System.out.println("===========================");
 
     }
-   
-    public static boolean isPerfectSquare(int n){
+
+    public static boolean isPerfectSquare(int n) {
         return Math.sqrt(n) % 1 == 0;
     }
 
