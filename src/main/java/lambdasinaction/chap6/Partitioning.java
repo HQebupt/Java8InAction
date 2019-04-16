e764de44525195063da2ff39f5c7ce8663fc4e36
HQebupt <qiang.huang1991@gmail.com>
2019-04-16 10:21:52 +0800

    update for partitioning function

--- src/main/java/lambdasinaction/chap6/Partitioning.java
+++ src/main/java/lambdasinaction/chap6/Partitioning.java
@@ -12,6 +12,8 @@ public class Partitioning {
         System.out.println("Dishes partitioned by vegetarian: " + partitionByVegeterian());
         System.out.println("Vegetarian Dishes by type: " + vegetarianDishesByType());
         System.out.println("Most caloric dishes by vegetarian: " + mostCaloricPartitionedByVegetarian());
+        System.out.println("Vegetarian Dishes by calories: " + vegetarianDishesByCalories());
+        System.out.println("Vegetarian Dishes by counting: " + vegetarianDishesCounting());
     }
 
     private static Map<Boolean, List<Dish>> partitionByVegeterian() {
@@ -21,6 +23,15 @@ public class Partitioning {
     private static Map<Boolean, Map<Dish.Type, List<Dish>>> vegetarianDishesByType() {
         return menu.stream().collect(partitioningBy(Dish::isVegetarian, groupingBy(Dish::getType)));
     }
+
+    private static Map<Boolean, Map<Boolean, List<Dish>>> vegetarianDishesByCalories() {
+        return  menu.stream().collect(partitioningBy(Dish:: isVegetarian, partitioningBy(d -> d.getCalories() > 500)));
+    }
+
+    private static Map<Boolean, Long> vegetarianDishesCounting() {
+        return  menu.stream().collect(partitioningBy(Dish:: isVegetarian, counting()));
+    }
+
 
     private static Object mostCaloricPartitionedByVegetarian() {
         return menu.stream().collect(
